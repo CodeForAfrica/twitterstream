f99a0728bc9ec1677533177c5bcf746f81802a25
Andrew Kamau <andkamau@gmail.com>
2016-08-15 23:44:42 +0300

    cleaner logging

--- streaming/start.py
+++ streaming/start.py
@@ -19,17 +19,17 @@ class Streamer(object):
                 listener.get_api(auth_only=True), self.stream_listener).filter(
                         follow=users).on_closed(
                                 self.stream_listener.on_dropped_connection())
-        #self.logger.info("Stream %s started." % self.stream_id)
+        self.logger.info("Stream %s started." % self.stream_id)
 
     def start_hashtag_stream(self, hashtags=[]):
         '''
         start stream following hashtag
         '''
         tweepy.Stream(listener.get_api(auth_only=True), self.stream_listener).filter(track=hashtags)
-        #self.logger.info("Stream %s started." % self.stream_id)
+        self.logger.info("Stream %s started." % self.stream_id)
 
 
-def main(stream_type, users=[], hashtags=[]):
+def main(stream_type, filters=[]):
     '''
     @stream_type:   <user / hashtag>
     @users:         <list> of users     (M when `stream_type` is 'user')
@@ -40,13 +40,14 @@ def main(stream_type, users=[], hashtags=[]):
         logging.basicConfig(filename=LOGGING['location'] % stream_type,
                 level=LOGGING['level'],
                 format=LOGGING['format'])
-        logging.info("Starting %s stream against %s %s" % (stream_type, users, hashtags))
+        logging.info("Starting %s stream against %s" % (stream_type, filters))
 
         stream = Streamer(logging)
+
         if stream_type == "user":
-            stream.start_user_stream(users)
+            stream.start_user_stream(filters.split(","))
         elif stream_type == "hashtag":
-            stream.start_hashtag_stream(users)
+            stream.start_hashtag_stream(filters.split(","))
 
     except AssertionError:
         print "ERROR: Invalid stream_type %s. Expects: ['user', 'hashtag']" % stream_type
